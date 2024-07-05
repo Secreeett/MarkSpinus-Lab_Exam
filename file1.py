02db15a1719bbd9e76f03922b9d0de685f19dc8b
nhojp <21-77155@g.batstate-u.edu.ph>
2024-07-05 10:49:42 +0800

    Fixed bug in Factorial function and added debug print statement to file1.py

--- file1.py
+++ file1.py
@@ -1,7 +1,11 @@
 def factorial(n):
-if n == 0:
-return 1
-else:
-return n * factorial(n-1)
+    if n == 0:
+        return 1
+    else:
+        result = n * factorial(n-)
+        print(f"factorial({n}) = {result}")
+        return = result
+
+
 result = factorial(5)
 print(f"Factorial of 5 is: {result}")
